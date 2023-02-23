03eac22db0a77aae5efba33b9ebbe6a33a49f967
Aliaksei Barsukov <barsecky@gmail.com>
2023-02-24 00:59:08 +0300

    feat: endet css style, add js files and slider js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './index.html';
 import './index.scss';
+import { slider } from './modules/slider.js'
+
+
+const init = () => {
+
+}
+
+
+init();
+
+window.addEventListener('DOMContentLoaded', slider)
